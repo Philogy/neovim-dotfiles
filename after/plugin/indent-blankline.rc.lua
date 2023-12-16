50dff01a07fbd85b97196fba919ac088b2d53c1a
Philippe Dumonet <philippe@dumo.net>
2023-12-15 22:01:35 -0500

    update indent-line

--- after/plugin/indent-blankline.rc.lua
+++ after/plugin/indent-blankline.rc.lua
@@ -1,8 +1,12 @@
-local status, indent_blankline = pcall(require, 'indent_blankline')
+local status, indent_blankline = pcall(require, 'ibl')
 if (not status) then return end
 
 indent_blankline.setup {
-  blankline_enabled = false,
-  show_trailing_blankline_indent = false,
-  char = '¦'
+  scope = {
+    enabled = false
+  },
+  whitespace = {
+    remove_blankline_trail = true,
+  },
+  indent = { char = '¦' }
 }
