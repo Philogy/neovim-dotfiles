757fa58a049a85e4b20663c713245ed83e4708d3
Philippe Dumonet <philippe@dumo.net>
2024-02-29 10:20:22 -0700

    Add balls helpers

---
+++ luasnip/balls.lua
@@ -0,0 +1,34 @@
+local ls = require 'luasnip'
+local s = ls.s --> snippet
+local i = ls.i --> insert node
+local t = ls.t --> text node
+local c = ls.choice_node
+local fmt = require 'luasnip.extras.fmt'.fmt
+local rep = require 'luasnip.extras'.rep
+
+return {
+  s(
+    'fn',
+    fmt(
+      [[
+fn {}({}) [{}] -> [{}] {{
+    {}
+}}
+      ]],
+      {
+        i(1),
+        i(2),
+        i(3),
+        i(4),
+        i(0)
+
+      }),
+    {}
+  ),
+  s(
+    'extern',
+    fmt('extern {}({}) stack({}, {}){}', { i(1), i(2), i(3), i(4), i(0) })
+  )
+}, {
+
+}
--- queries/balls/highlights.scm
+++ queries/balls/highlights.scm
@@ -1,6 +1,8 @@
 (comment) @comment
 
 (number) @number
+(decimal_number) @number
+(hex_number) @number
 
 [
   ","
@@ -20,17 +22,37 @@
 
 [
   "const"
-  "extern"
   "dependency"
 ] @type
 
+[
+ "reads"
+ "writes"
+ "stack"
+] @type
 
-"fn" @keyword.function
+
+(huff_const . (identifier) @constant)
+(abstract_dependency . (identifier) @constant)
+
+[
+ "fn"
+ "extern"
+] @keyword.function
 
 (function_definition . (identifier) @function)
+
+(extern_macro . (identifier) @function)
+
 
 (std_evm_op) @function.builtin
 
 (invocation . (identifier) @function.call)
 
-(identifier) @variable
+(reads
+  (ident_list
+    (identifier) @constant))
+
+(writes
+  (ident_list
+    (identifier) @constant))
